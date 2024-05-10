77a76605f7a667f72a218097e73119edf6265c38
Dennys González <dgonzalez870@yahoo.com>
2024-05-10 20:49:41 +0200

    feat: add properties to the employee model

--- backend/app/Models/Employee.php
+++ backend/app/Models/Employee.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'first_surname',
+        'second_surname',
+        'first_name',
+        'other_names',
+        'job_country',
+        'id_document',
+        'id_number',
+        'email',
+        'admission_date',
+        'job_area',
+        'status',
+    ];
 }
--- backend/database/migrations/2024_05_10_160951_create_employees_table.php
+++ backend/database/migrations/2024_05_10_160951_create_employees_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -13,7 +14,19 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('first_surname', 20);
+            $table->string('second_surname', 20);
+            $table->string('first_name', 20);
+            $table->string('other_names', 50)->nullable(true);
+            $table->bigInteger('job_country');
+            $table->bigInteger('id_document');
+            $table->string('id_number', 20);
+            $table->string('email', 300);
+            $table->date('admission_date');
+            $table->bigInteger('job_area');
+            $table->string('status')->default('Activo');
+            $table->timestamp('created_at', 0)->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at', 0)->default(DB::raw('CURRENT_TIMESTAMP'))->onUpdate(DB::raw('CURRENT_TIMESTAMP'));
         });
     }
 
