faaecd395223ac503a7064b7a7858c661d7c22df
Dennys González <dgonzalez870@yahoo.com>
2024-05-10 12:45:30 +0200

    feat: add countries controller scaffolding

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\CountriesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('countries')->group(function () {
+    Route::get('/', [CountriesController::class, 'exec']);
+});
