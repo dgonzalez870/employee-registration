00e27bec11eb89b71318d9b0fc51f82db247fd3d
Dennys González <dgonzalez870@yahoo.com>
2024-05-10 17:04:48 +0200

    feat: add queries in countries module

--- backend/database/migrations/2024_05_10_105554_create_countries_table.php
+++ backend/database/migrations/2024_05_10_105554_create_countries_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('countries', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name', 100);
+            $table->string('code', 3);
         });
     }
 
