046034be6991250d728867a83b6b521a6e037dfe
Dennys González <dgonzalez870@yahoo.com>
2024-05-11 13:52:39 +0200

    feat: add error handler

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -1,14 +1,18 @@
 <?php
 
+use GuzzleHttp\Psr7\Message;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -16,4 +20,23 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
+        $exceptions->render(function (Throwable $e, Request $request) {
+
+            if ($e instanceof HttpException) {
+                return response()->json([
+                    'message' => $e->getMessage()
+                ], $e->getStatusCode());
+            }
+
+            if ($e instanceof ValidationException) {
+                return response()->json([
+                    'message'   => 'validation error',
+                    'validator' => $e->validator->errors()->getMessages(),
+                ], 400);
+            }
+
+            return response()->json(
+                ['message' => $e->getMessage()]
+            );
+        });
     })->create();
