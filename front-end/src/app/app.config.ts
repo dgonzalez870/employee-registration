8a909bed29eeae7ed909ec786e927a5c38645d23
Dennys González <dgonzalez870@yahoo.com>
2024-05-14 11:46:11 +0200

    feat: add error handler

--- front-end/src/app/app.config.ts
+++ front-end/src/app/app.config.ts
@@ -1,9 +1,16 @@
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
+import {
+  ApplicationConfig,
+  ErrorHandler,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { ErrorHandlerService } from './lib/error-handler/error-handler.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()],
+  providers: [provideRouter(routes), provideHttpClient(), {
+    provide: ErrorHandler,
+    useExisting: ErrorHandlerService,
+  }],
 };
---
+++ front-end/src/app/lib/error-handler/error-handler.service.ts
@@ -0,0 +1,15 @@
+import {
+  ErrorHandler,
+  Injectable,
+} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ErrorHandlerService implements ErrorHandler {
+  constructor() {}
+
+  handleError(error: any): void {
+    console.log(error);
+  }
+}
